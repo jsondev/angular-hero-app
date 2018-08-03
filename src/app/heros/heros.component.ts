e48f89b562216af319f54ce2f63459f89ea2e03d
James Anderson <jsondev1.0@outlook.com>
2018-08-03 07:56:17 -0400

    Created hero class and added object to component

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heros',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HerosComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero:Hero = {
+    id:1,
+    name: 'Windstorm'
+  }
 
   constructor() { }
 
